eb1f9b01aa1c76d0a627175a1fb968fc240938e7
Lucian Pantiru <lucian.t.pantiru@gmail.com>
2018-02-20 05:12:41 +0200

    make appointments great again (i mean editable)

--- dx17test/dx17test/Controllers/HomeController.cs
+++ dx17test/dx17test/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using DevExpress.Web.Mvc;
+using dx17test.EFModels;
 using dx17test.Helpers;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,7 @@ namespace dx17test.Controllers
 {
     public class HomeController : Controller
     {
+        #region reading
         public ActionResult Index()
         {
             return View(SchedulerDataHelper.DataObject);
@@ -18,6 +21,41 @@ namespace dx17test.Controllers
         {
             return PartialView("SchedulerPartial", SchedulerDataHelper.DataObject);
         }
+        #endregion
+
+        #region editing
+
+        public ActionResult EditAppointment()
+        {
+            UpdateAppointment();
+            return PartialView("SchedulerPartial", SchedulerDataHelper.DataObject);
+        }
+
+        static void UpdateAppointment()
+        {
+            DBAppointment[] insertedAppointments = SchedulerExtension.GetAppointmentsToInsert<DBAppointment>("scheduler", SchedulerDataHelper.GetAppointments(),
+                SchedulerDataHelper.GetResources(), SchedulerStorageProvider.DefaultAppointmentStorage, SchedulerStorageProvider.DefaultResourceStorage);
+            foreach (var appt in insertedAppointments)
+            {
+                AppointmentDataAccessor.InsertAppointment(appt);
+            }
+
+            DBAppointment[] updatedAppointments = SchedulerExtension.GetAppointmentsToUpdate<DBAppointment>("scheduler", SchedulerDataHelper.GetAppointments(),
+                SchedulerDataHelper.GetResources(), SchedulerStorageProvider.DefaultAppointmentStorage, SchedulerStorageProvider.DefaultResourceStorage);
+            foreach (var appt in updatedAppointments)
+            {
+                AppointmentDataAccessor.UpdateAppointment(appt);
+            }
+
+            DBAppointment[] removedAppointments = SchedulerExtension.GetAppointmentsToRemove<DBAppointment>("scheduler", SchedulerDataHelper.GetAppointments(),
+                SchedulerDataHelper.GetResources(), SchedulerStorageProvider.DefaultAppointmentStorage, SchedulerStorageProvider.DefaultResourceStorage);
+            foreach (var appt in removedAppointments)
+            {
+                AppointmentDataAccessor.RemoveAppointment(appt);
+            }
+        }
+
+        #endregion
     }
 }
 
